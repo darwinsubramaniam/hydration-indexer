19a66c326d8e7a164e9338d9149f6d9cce8d67d3
Darwin Subramaniam <darwinsubramaniam@gmail.com>
2025-07-27 11:24:10 +0800

    Validate and sanitize EVM address conversion

--- src/utils/evmToSubstrate.utils.ts
+++ src/utils/evmToSubstrate.utils.ts
@@ -1,10 +1,15 @@
-import { hexToU8a, u8aToU8a } from '@polkadot/util';
-import { encodeAddress } from '@polkadot/util-crypto';
+import assert from 'assert'
+import { hexToU8a } from '@polkadot/util'
+import { encodeAddress } from '@polkadot/util-crypto'
 
 /// Convert the EVM address to Substrate address
 /// Default ss58Prefix is 0
-export function evmToSubstrate(address: string, ss58Prefix=0): string {
-    let u8a = hexToU8a(address);
-    return encodeAddress(u8a,ss58Prefix);
+export function evmToSubstrate(address: string, ss58Prefix = 0): string {
+    if (address.startsWith('0x')) {
+        address = address.slice(2)
+    }
+    assert(address.length === 40, 'Invalid EVM address')
+    const u8a = hexToU8a(address)
+    return encodeAddress(u8a, ss58Prefix)
 }
     
